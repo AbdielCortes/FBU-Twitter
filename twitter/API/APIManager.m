ffb64761865c0f7a8ea3e519a63cd410011128bb
Abdiel Cortes <zurken@fb.com>
2020-06-29 15:45:19 -0700

    created user class

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -9,8 +9,8 @@
 #import "APIManager.h"
 
 static NSString * const baseURLString = @"https://api.twitter.com";
-static NSString * const consumerKey = // Enter your consumer key here
-static NSString * const consumerSecret = // Enter your consumer secret here
+static NSString * const consumerKey = @"9XJO9d7lcW24mdVEVXfavKTk3";
+static NSString * const consumerSecret = @"JNFZffHVJ6JSL2DMWaPUhgEmQzAIGUGumhR64mbHUo0ce1Bp1N";
 
 @interface APIManager()
 
